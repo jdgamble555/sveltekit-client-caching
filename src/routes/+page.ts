069a5c01cb1dd2e2637933fbaae85d406c5b59b2
Jonathan Gamble <jdgamble555@gmail.com>
2023-11-21 07:58:14 -0600

    cache all posts

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,12 +1,17 @@
 import type { PageLoad } from "./$types";
-import { cacheFetch } from "$lib/cache";
+import { /*cache,*/ cacheFetch } from "$lib/cache";
 
 export const load = (async ({ fetch, url }) => {
 
     const { pathname } = url;
 
+    const posts = await cacheFetch<Post[]>(pathname, () => fetch(`/data`));
+
+    // cache all posts immediately
+    // posts.map(post => cache.set(`${pathname}post/${post.id}`, post));
+
     return {
-        posts: cacheFetch<Post[]>(pathname, () => fetch(`/data`))
+        posts
     };
 
 }) satisfies PageLoad;
